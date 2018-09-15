ae1112b249345e0fd78d4957ff9f9a14a0a62387
Ebillson GRAND JEAN <ebillsongj@Jamess-Air.workgroup>
2018-09-14 23:55:41 -0400

    First commit

---
+++ TumblrFeedApp/View Controller/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCellTableViewCell.swift
+//  TumblrFeedApp
+//
+//  Created by Ebillson GRAND JEAN on 9/14/18.
+//  Copyright © 2018 Ebillson GRAND JEAN. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
