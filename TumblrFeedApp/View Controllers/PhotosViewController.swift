6be1b568c64a367bb83563fab63b51629af09683
Ebillson GRAND JEAN <ebillsongj@Jamess-Air.workgroup>
2018-09-16 12:46:32 -0400

    fixed Tableview row height

--- TumblrFeedApp/View Controllers/PhotosViewController.swift
+++ TumblrFeedApp/View Controllers/PhotosViewController.swift
@@ -24,6 +24,7 @@ class PhotosViewController: UIViewController, UITableViewDataSource, UITableView
         
         tableview.delegate = self
         tableview.dataSource = self
+        tableview.rowHeight = 200
         
         // Network request snippet
         let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
